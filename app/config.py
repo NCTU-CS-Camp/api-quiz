8e4a4ba905ec32df88b104d6e8c5bbff3dda7569
shangjung <david103132881@gmail.com>
2025-06-23 00:34:23 +0800

    keep only quiz scripts and questions

--- app/config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '../data.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SECRET_KEY = os.getenv('SECRET_KEY', 'dev_key')
--- app/extensions.py
+++
@@ -1,7 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-from flask_httpauth import HTTPBasicAuth
-from flask_socketio import SocketIO
-
-db   = SQLAlchemy()
-auth = HTTPBasicAuth()
-socketio = SocketIO(cors_allowed_origins="*")
